140368d80b59ccdea3cdebe7e4c9287231f3fc85
SAIKOT-ROY <saikotroydev@gmail.com>
2024-11-26 22:07:26 +0600

    hero section is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <main>
-      <h1 className="font-robert-regular text-4xl text-violet-300">Hello World</h1>
-    </main>
-  )
-}
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
 
-export default App
+      <section  className="z-0 min-h-screen bg-blue-500"/>
+    </main>
+  );
+};
+
+export default App;
